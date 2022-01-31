aab23b2975e099c95a5f54a7e647a7359a4baffa
virgolootus <98746072+virgolootus@users.noreply.github.com>
2022-01-31 13:37:11 +0200

    Add files via upload

---
+++ Harjutus01.py
@@ -0,0 +1,17 @@
+#Harjutus 01
+#Virgo Lootus
+#31.01.2022
+
+#kogu tekst ühel real
+print("\"Cabernet Sauvigon\" või 'Cabernet Sauvigon'?")
+
+#konna joonistus
+print("  @..@")
+print(" (----)")
+print("( \__/ )")
+print('^^ "" ^^')
+
+#ruudu pindala
+a = int(input("sisesta ruudu külejpikkus: "))
+s = a * a
+print("Vastus: euudu pindala on ", str(s)+"cm⌃2")
