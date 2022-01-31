8110d2a418c9572b1dd6423e338ea7cefbedaa53
virgolootus <98746072+virgolootus@users.noreply.github.com>
2022-01-31 13:59:54 +0200

    Add files via upload

---
+++ Harjutus02.py
@@ -0,0 +1,21 @@
+#Ülesanne 02
+#Virgo Lootus
+#31.01.2022
+
+#Toote hinna arvutamine
+
+Hind = 36.75
+Soodukas = 40
+Arvutus = (36.75 / 40 * 100)*3 
+print("Vastus: kolme toote summa on ", 
+
+
+
+
+
+#Kolmnurga ümbermõõt
+a = int(input("Sisestage külje a pikkus: "))
+b = int(input("Sisestage külje b pikkus: "))
+c = int(input("Sisestage külje c pikkus: "))
+P = a + b + c
+print("Vastus: kolmnurga ümbermõõt on ", str(P)+"cm⌃2")
