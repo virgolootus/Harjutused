d08a802dbccfeed7b3e2f1190f27d75179dd0534
virgolootus <98746072+virgolootus@users.noreply.github.com>
2022-02-09 15:42:06 +0200

    Add files via upload

--- Harjutus04.py
+++ Harjutus04.py
@@ -2,12 +2,65 @@
 #Virgo Lootus
 #03.02.2022
 
-#korrutustabel
+#pank
+konto = 0
+raha = int(input("sisestage summa: "))
+aastad = int(input("sisestage aastad: "))
+intress = 0.05
+konto += raha
 
-
+print(f"{'Aasta':4} {'Algsumma':10} {'Intress':10} {'Aasta lõpuks':10}")
+for a in range(aastad):
+    kasum = konto*intress
+    print(f"{a+1:4} {konto:10.2f} {kasum:10.2f} {konto+kasum:10.2f}")
+    konto+=kasum
+    
 
 
 '''
+#arvamusmäng
+
+import random
+
+loop = 1
+korrad = 1
+suvaline = random.randint(1,10)
+while loop == 1:
+    if korrad <= 3:
+        arv = int(input("arva arv 1-10: "))
+    else:
+        veel = input("tahad veel mängida? jah/ei: ")
+        if veel =="jah":
+            korrad = 0
+        else:
+            loop = 0
+    korrad += 1
+    if arv == suvaline:
+        print("tubli, õige")
+    else:
+        print("ei arvand ära")
+
+    print(suvaline, arv)
+print("mäng läbi")    
+
+
+#viisikud
+
+for a in range(1,101):
+    jaak = a%int(5)
+    if jaak == 0:
+        print(a)
+    else:
+        ()
+
+
+#korrutustabel
+
+for a in range(1,11):
+    print(f"5 x {a} = {a*5}")
+
+
+#paaris ja paaritu
 for p in range(1,101):
     
 
@@ -26,7 +79,6 @@ import random
 for e in range(1,6):
     a = int(random.randint(0,9))
     print (a,end="")
-
 
 
 #tärnid
