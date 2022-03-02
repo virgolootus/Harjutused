32691efddf6e7974bb996f741a6e04897a5d01b4
virgolootus <98746072+virgolootus@users.noreply.github.com>
2022-03-02 14:31:35 +0200

    Add files via upload

--- 22.py
+++ 22.py
@@ -78,4 +78,31 @@ korrad = int(input("mitu korda peaks äratama: "))
 
 for a in range(korrad):
     print("Tõuse ja sära")
-    
+#1.4
+def bussid():
+    inimesed = int(input("Sisesta inimeste arv: "))
+    istekohad = int(input("Sisesta bussi kohtade arv: "))
+    viimane = inimesed%istekohad
+    if viimane == 0:
+        bussid = inimesed//istekohad
+        viimane = istekohad
+    else:
+        bussid = inimesed//istekohad+1
+    print(f"Busse vaja: {bussid}\nViimases bussis inimesi: {viimane}")
+
+bussid()
+
+#1.2
+def liblikas():
+    aasta = 2020
+    liblikas = "teelehe-mosaiikliblikas"
+    lause_keskosa = ". aasta liblikas on"
+    lause = str(aasta)+lause_keskosa+liblikas
+    print(lause)
+liblikas()
+#1.1
+
+def tere():
+    print("Tere, maailm!")
+tere()
+
